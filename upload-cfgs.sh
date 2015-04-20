1cde81c94f10778662e19929708cb77dacf71b8d
Alexander Fefelov <alexanderfefelov@yandex.ru>
2015-04-20 06:06:25 +0300

    Minor refactoring

--- upload-cfgs.sh
+++ upload-cfgs.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+USERNAME=admin
+PASSWORD=admin
+TFTP=10.10.10.10
+
+# DO NOT CHANGE BELOW THIS LINE
+
 while read s; do
   [[ "$s" =~ ^#.*$ ]] && continue
-  ./upload-cfg.exp $s admin admin 10.10.10.10
+  ./upload-cfg.exp $s $USERNAME $PASSWORD $TFTP
 done < switches
