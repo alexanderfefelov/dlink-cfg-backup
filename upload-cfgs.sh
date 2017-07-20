63c4c8574acc8f8de2a2945d700acf78c4bf170c
Alexander Fefelov <alexanderfefelov@yandex.ru>
2017-07-20 09:25:01 +0300

    Fix all issues found by shellcheck.net

--- upload-cfgs.sh
+++ upload-cfgs.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-USERNAME=admin
-PASSWORD=admin
-TFTP=10.10.10.10
+USERNAME="admin"
+PASSWORD="admin"
+TFTP="10.10.10.10"
 
 # DO NOT CHANGE BELOW THIS LINE
 
 # http://stackoverflow.com/a/246128
-HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-LOG=$HOME/`basename $0`.log
+LOG="$HOME"/$(basename "$0").log
 
-while read s; do
+while read -r s; do
   [[ "$s" =~ ^#.*$ ]] && continue
-  date >> $LOG
-  $HOME/upload-cfg.exp $s $USERNAME $PASSWORD $TFTP >> $LOG
-  echo -------------------------------------------------------------------------------- >> $LOG
-done < $HOME/switches
+  {
+    date
+    "$HOME"/upload-cfg.exp "$s" $USERNAME $PASSWORD $TFTP
+    echo --------------------------------------------------------------------------------
+  } >> "$LOG"
+done < "$HOME"/switches
